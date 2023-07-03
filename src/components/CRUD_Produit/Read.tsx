ff5dcfcf45e14657afe10bc72fb05cdfaf68539f
Dairalir <daimanvm@gmail.com>
2023-07-03 09:04:21 +0200

    modif axios

--- src/components/CRUD_Produit/Read.tsx
+++ src/components/CRUD_Produit/Read.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 
@@ -6,34 +7,30 @@ interface Produit {
     name: string;
     description: string;
     price: string;
-    picture : string;
+    picture: string;
     stock: number;
-    active : boolean;
+    active: boolean;
     sous_rubrique: [string];
 }
 
-export function ReadProduit(){
+export function ReadProduit() {
     const [produits, setProduits] = useState<Produit[]>([])
 
     const produitData = () => {
-        fetch(`https://damienvm.amorce.org/api/produits`, {
-            headers : {
-                "Accept": "application/json",
-            },
-        })
-        .then(response => response.json())
-        .then((json) => setProduits(json))
-        .catch(error => console.error(error))
+        axios.get(`https://damienvm.amorce.org/api/produits`)
+            .then((r) => {
+                setProduits(r.data["hydra:member"])
+            })
     }
 
     useEffect(() => {
         produitData()
-    },[])  
+    }, [])
 
-    return(
+    return (
         <div>
             <h1>Produits</h1>
-            {produits.map(produit =>(
+            {produits.map(produit => (
                 <div key={produit.id}>
                     <h3>{produit.name}</h3>
                     <b>Description :</b>
@@ -48,4 +45,3 @@ export function ReadProduit(){
     )
 }
 
-    
