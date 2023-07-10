72c988c357c60e332831cd00cb798f60c3e5f02d
Dairalir <daimanvm@gmail.com>
2023-07-10 15:06:04 +0200

    graph

--- src/components/Graphs/Graph1.tsx
+++ src/components/Graphs/Graph1.tsx
@@ -9,9 +9,21 @@ export function Graph1() {
         ["Batiment", 3],
         ["Industrie", 7]
     ]);
+    const [data1, setData1] = useState([
+        ["Evénements", "Nombre"],
+        ["Tertiaire", 11],
+        ["Batiment", 3],
+        ["Industrie", 7]
+    ]);
 
-    const options = {
-        title: "Evénements par secteur",
+
+    const option1 = {
+        title: "CA mois par mois sur année",
+        is3D: true
+    };
+
+    const option2 = {
+        title: "CA par fournisseur",
         is3D: true
     };
 
@@ -20,7 +32,14 @@ export function Graph1() {
             <Chart
                 chartType="PieChart"
                 data={data}
-                options={options}
+                options={option1}
+                width={"100%"}
+                height={"400px"}
+            />
+            <Chart
+                chartType="PieChart"
+                data={data1}
+                options={option2}
                 width={"100%"}
                 height={"400px"}
             />
